a140114b00673343799a6ea26616746293fea487
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-13 13:26:45 +0800

    Exercise 2

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,19 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return "Hello Laravel!";
 });
+
+// > Exercuse 2
+// ? About
+Route::get('/about', function() {
+    return "This is About Page";
+});
+
+// ? Contact
+Route::get('/contact', function () {
+    return "This is Contact Page";
+});
+
+// ? Greet/{name}
+Route::get('/greet/{name}', function ($name) {
+    return "Hello, {$name}";
+});
